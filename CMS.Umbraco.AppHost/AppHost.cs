b11221a5737dde2b6cbc720e118cea4a7f11dd95
Rex77pro <smaurieka@gmail.com>
2026-05-04 15:08:49 +0200

    revert

--- CMS.Umbraco.AppHost/AppHost.cs
+++ CMS.Umbraco.AppHost/AppHost.cs
@@ -1,15 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var compose = builder.AddDockerComposeEnvironment("compose")
-                     .WithProperties(env =>
-                     {
-                         env.DashboardEnabled = true;
-                     });
-
-var umbraco = builder.AddProject<Projects.CMS_Umbraco>("cms-umbraco")
-                     .PublishAsDockerComposeService((resource, service) =>
-                     {
-                         service.Name = "umbraco";
-                     });
+builder.AddProject<Projects.CMS_Umbraco>("cms-umbraco");
 
 builder.Build().Run();
