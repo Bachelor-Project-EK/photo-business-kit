e45b58f14e9e3a9b7f7b4e70e8e2be93facf3b6e
Brezzor <oliver.bresson@gmail.com>
2026-05-21 15:29:28 +0200

    Added basic test project for extension

---
+++ Umbraco.Extension.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Umbraco.Extension.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
