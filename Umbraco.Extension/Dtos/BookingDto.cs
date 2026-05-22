a8788ad8c98370e08a120162b065e4ecee617e45
Brezzor <oliver.bresson@gmail.com>
2026-05-22 15:00:32 +0200

    initial setup for api.

---
+++ Umbraco.Extension/Dtos/BookingDto.cs
@@ -0,0 +1,27 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Umbraco.Cms.Infrastructure.Persistence.DatabaseAnnotations;
+using Umbraco.Extension.Enum;
+using Umbraco.Extension.Models;
+
+namespace Umbraco.Extension.Dtos
+{
+    public class BookingDto
+    {
+        public required DateTimeOffset CreatedOn { get; set; }
+
+        public required DateTimeOffset UpdatedOn { get; set; }
+
+        public required DateTimeOffset StartDate { get; set; }
+
+        public required DateTimeOffset EndDate { get; set; }
+
+        public required BookingStatus Status { get; set; }
+
+        public string? Comment { get; set; }
+
+        //public PhotoPackages? PhotoPackage { get; set; }
+    }
+}
