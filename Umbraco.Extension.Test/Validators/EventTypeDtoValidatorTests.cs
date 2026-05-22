b517fa1172762982eef7ccf4df95794e9d12ed81
Rex77pro <smaurieka@gmail.com>
2026-05-22 18:23:06 +0200

    adds three validator tests

---
+++ Umbraco.Extension.Test/Validators/EventTypeDtoValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.TestHelper;
+using Umbraco.Extension.Dtos;
+using Umbraco.Extension.Validators;
+
+namespace Umbraco.Extension.Test.Validators;
+
+public class EventTypeDtoValidatorTests
+{
+    private EventTypeDtoValidator _validator = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _validator = new EventTypeDtoValidator();
+    }
+
+    [Test]
+    public void EventTypeDto_should_have_validation_errors()
+    {
+        // Arrange
+        var dto = new EventTypeDto
+        {
+            EventTypeName = ""
+        };
+
+        // Act
+        var result = _validator.TestValidate(dto);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.EventTypeName);
+    }
+
+    [Test]
+    public void EventTypeDto_should_not_have_validation_errors()
+    {
+        // Arrange
+        var dto = new EventTypeDto
+        {
+            EventTypeName = "Wedding"
+        };
+
+        // Act
+        var result = _validator.TestValidate(dto);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
